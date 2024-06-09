dd896dca163e407fa04b085403d41a29a7d390a6
Abdullah Naeem <raoabdullahn@gmail.com>
2024-06-09 13:31:47 +0500

    Add validation checks when registering a new user

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ connectToMongo();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../Models/User");
 
+const {
+  query,
+  matchedData,
+  validationResult,
+  body,
+} = require("express-validator");
 
-router.get('/', (req, res)=>{
-    res.send("Hello Auth!");
-});
-
+router.post(
+  "/register",
+  body("name", "Name Must be atleast 3 characters long.").isLength({ min: 3 }),
+  body("email", "Please Enter a valid email.").isEmail(),
+  body("password", "Password Must be atleast 5 characters long.").isLength({
+    min: 5,
+  }),
+  (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    } else {
+      User.create(
+        {
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        })
+        .then(user => res.send(user))
+        .catch(err => {
+            res.json({
+                message: "This email is already registered..",
+                error: err.errmsg
+            });
+        });
+    }
+  }
+);
 
 module.exports = router;
