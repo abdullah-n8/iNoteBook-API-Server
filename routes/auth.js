75bbac1a2c431ccd4b2579d8616cd597443f18b2
Abdullah Naeem <raoabdullahn@gmail.com>
2024-06-09 13:56:17 +0500

    refactor and add some more checks when adding user

--- routes/auth.js
+++ routes/auth.js
@@ -1,41 +1,49 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../Models/User");
+// This file contains the routes for handling user authentication
 
-const {
-  query,
-  matchedData,
-  validationResult,
-  body,
-} = require("express-validator");
+// Importing necessary modules
+const express = require("express"); // Express framework
+const router = express.Router(); // Creating a new router instance
+const User = require("../Models/User"); // Importing the User model
 
+// Importing validation middlewares
+const { validationResult, body } = require("express-validator"); // Express validation middleware
+
+// Creating a POST route for user registration
 router.post(
-  "/register",
-  body("name", "Name Must be atleast 3 characters long.").isLength({ min: 3 }),
-  body("email", "Please Enter a valid email.").isEmail(),
-  body("password", "Password Must be atleast 5 characters long.").isLength({
-    min: 5,
-  }),
+  "/registeruser", // Endpoint for registering a new user
+  
+  // Validating the request body
+  body("name", "Name Must be atleast 3 characters long.").isLength({ min: 3 }), // Validating name field
+  body("email", "Please Enter a valid email.").isEmail(), // Validating email field
+  body("password", "Password Must be atleast 5 characters long.").isLength({ min: 5 }), // Validating password field
+  
+  // Handling the request
   (req, res) => {
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      return res.status(400).json({ errors: result.array() });
-    } else {
-      User.create(
-        {
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-        })
-        .then(user => res.send(user))
-        .catch(err => {
-            res.json({
-                message: "This email is already registered..",
-                error: err.errmsg
-            });
-        });
+    
+    // Validating the request
+    const result = validationResult(req); // Validating the request body
+    if (!result.isEmpty()) { // If there are any validation errors
+      return res.status(400).json({ errors: result.array() }); // Returning the validation errors as a JSON response
     }
+
+    // Creating a new user
+    User.create(
+      {
+        name: req.body.name, // Setting the name from the request body
+        email: req.body.email, // Setting the email from the request body
+        password: req.body.password, // Setting the password from the request body
+      })
+      .then(user => res.send(user)) // If the user is created successfully, sending the user object as a response
+      .catch(err => {
+        if (err.code === 11000) { // If there is a duplicate key error (email already exists)
+          res.json({
+            message: "A user with this email already exists",
+            error: err.errmsg
+          }); // Returning a JSON response with the error message
+        }
+      });
   }
 );
 
+// Exporting the router
 module.exports = router;
